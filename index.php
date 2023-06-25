87ba7832808bdda2877614d319010ed1382371c1
Сергей <78095833+RuCoder-sudo@users.noreply.github.com>
2023-06-25 15:29:33 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Поиск упоминаний в Вконтакте</title>
+</head>
+<body>
+<h1>Поиск упоминаний в социальной сети Вконтакте</h1>
+<form method="post" action="search.php"> <label>Введите поисковый запрос:</label> <input type="text" name="query" required> <br><br> <label>От:</label> <input type="date" name="from_date" required> <label>До:</label> <input type="date" name="to_date" required> <br><br> <input type="submit" value="Поиск"> </form>
+</body>
+</html>
