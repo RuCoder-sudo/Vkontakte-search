98215a36b30ea781dc3a5bac6c69fe83b3f3791b
Сергей <78095833+RuCoder-sudo@users.noreply.github.com>
2023-06-25 15:29:09 +0300

    Create config.php
    config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('VK_API_VERSION', '5.130');         // версия API Вконтакте
+define('VK_API_ENDPOINT', 'https://api.vk.com/method/');   // адрес эндпоинта
+define('VK_API_ACCESS_TOKEN', 'Ваш токен');   // токен доступа пользователя
+define('VK_API_CLIENT_ID', 'ваш идентификатор приложения');   // идентификатор приложения
+define('VK_API_CLIENT_SECRET', 'ваш секретный ключ приложения');   // секретный ключ приложения
+?>
